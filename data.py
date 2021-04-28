a6a4488fcfb05de718902967c4fd468e3e0bc547
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2021-04-28 15:34:00 +0200

    R and T histogram titles updated

--- data.py
+++ data.py
@@ -18,14 +18,14 @@ class Data:
         tally.GetAxis(0).SetRange(1,1); # surf 1
         tally.GetAxis(5).SetRangeUser(-1.0, 0.0) # back
         self.R = tally.Projection(5,6)
-        self.R.SetNameTitle("R", "Reflected %s;Energy [MeV];cos(#mu)" % tally.GetTitle())
+        self.R.SetNameTitle("R", "Reflected %s;Energy [MeV];#mu" % tally.GetTitle())
         self.R.SetDirectory(0)
 
-        # Transported
+        # Transmitted
         tally.GetAxis(0).SetRange(2,2); # surf 2
         tally.GetAxis(5).SetRangeUser(0.0, 1.0) # forward
         self.T = tally.Projection(5,6)
-        self.T.SetNameTitle("T", "Transported %s;Energy [MeV];cos(#mu)" % tally.GetTitle())
+        self.T.SetNameTitle("T", "Transmitted %s;Energy [MeV];#mu" % tally.GetTitle())
         self.T.SetDirectory(0)
 
         f.Close()
