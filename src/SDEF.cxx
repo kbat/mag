257880a89ec701687ac27af1f56d310c36514c5d
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2021-06-02 09:12:22 +0200

    SDEF::SDEF: try/catch optimised

--- src/SDEF.cxx
+++ src/SDEF.cxx
@@ -25,22 +25,22 @@ SDEF::SDEF(const std::string& fname) :
       const std::string hname = f1->GetName();
       std::string tnum = hname;
       tnum.erase(0,1); // remove ^f
-      if (f1->GetAxis(0)->GetNbins() == 2) {
-	f1->GetAxis(0)->SetRange(2,2); // assume that 2nd bin is forward-going
-      }
-      f1->GetAxis(5)->SetRangeUser(epsilon, 1.0-epsilon); // forward bins only
-      auto h = f1->Projection(5,6);
-      h->SetDirectory(0);
       try {
 	const char p0 = tallyDict.at(std::stoi(tnum));
 	//	h->SetName(std::string(1,p0).c_str());
+	if (f1->GetAxis(0)->GetNbins() == 2) {
+	  f1->GetAxis(0)->SetRange(2,2); // assume that 2nd bin is forward-going
+	}
+	f1->GetAxis(5)->SetRangeUser(epsilon, 1.0-epsilon); // forward bins only
+	auto h = f1->Projection(5,6);
+	h->SetDirectory(0);
 	h->SetName(&p0);
 	//	std::cout << h->GetName() << std::endl;
 	sdef.insert(std::make_pair(p0, std::make_shared<TH2D>(*h)));
+	delete h; h = nullptr;
       } catch (...) {
-      	std::cout << "solve.cxx: error with " << hname << std::endl;
+      	std::cout << "SDEF::SDEF: skipping tally " << hname << std::endl;
       }
-      delete h; h = nullptr;
     }
     delete obj; obj = nullptr;
   }
