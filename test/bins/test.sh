3f08ca7f139a0d8485777f896de35c0f6a40cbb3
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2023-09-11 13:38:05 +0200

    test.sh: mctal.template path updated

--- test/bins/test.sh
+++ test/bins/test.sh
@@ -12,7 +12,7 @@ A="  1  0.0  2  0.0   3 0.0  4 0.0   0.00000E+00 0.0\n \
  -15 0.0  -16 0.0  -17 0.0  -18.1 0.0  0.00000E+00 0.0\n \
  11 0.0  12 0.0  13 0.0  14 0.0  0.00000E+00 0.0\n \
  15 0.0  16 0.0  17 0.0  18 0.0  0.00000E+00 0.0 0.00000E+00 0.0"
-cat mctal.template | sed "s;^VALUES;$A;" > mctal
+cat ../../../test/bins/mctal.template | sed "s;^VALUES;$A;" > mctal
 
 rm -fr case*
 mctal2root mctal
