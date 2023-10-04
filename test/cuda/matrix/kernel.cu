90242e790aaf33a93011f1bf9017557989a0de9b
Konstantin Batkov <konstantin.batkov@maxiv.lu.se>
2023-10-04 12:42:45 +0200

    tmpSum type changed to double

--- test/cuda/matrix/kernel.cu
+++ test/cuda/matrix/kernel.cu
@@ -11,15 +11,15 @@ __global__ void matrixMultiplicationKernel(float* A, float* B, float* C, int N) 
     int ROW = blockIdx.y*blockDim.y+threadIdx.y;
     int COL = blockIdx.x*blockDim.x+threadIdx.x;
 
-    float tmpSum = 0;
+    double sum = 0.0;
 
     if (ROW < N && COL < N) {
         // each thread computes one element of the block sub-matrix
         for (int i = 0; i < N; i++) {
-            tmpSum += A[ROW * N + i] * B[i * N + COL];
+            sum += A[ROW * N + i] * B[i * N + COL];
         }
     }
-    C[ROW * N + COL] = tmpSum;
+    C[ROW * N + COL] = sum;
 }
 
 
